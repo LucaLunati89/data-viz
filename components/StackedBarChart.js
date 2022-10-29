613e616a415decddd71b4ff02c517cf873821d46
Luca <80696098+LucaLunati89@users.noreply.github.com>
2022-10-29 17:59:31 +0200

    add fetch get group

--- components/StackedBarChart.js
+++ components/StackedBarChart.js
@@ -12,9 +12,9 @@ export default class StackedBarChart{
   //moltiplica o divide per una costante il valore totale del gruppo e i parziali delle categorie
   scaleValues(k, down) {
     this.mappedValues = new Map();
-    this.mappedValues.set(this.data.group.totalValue, 
-      down ? this.data.group.totalValue / k : this.data.group.totalValue * k);
-    this.data.group.categories.forEach((d) => {
+    this.mappedValues.set(this.data.totalValue, 
+      down ? this.data.totalValue / k : this.data.totalValue * k);
+    this.data.categories.forEach((d) => {
       this.mappedValues.set(d.value, 
         down ? d.value / k : d.value * k);
     });
@@ -25,13 +25,13 @@ export default class StackedBarChart{
     const isNotMappedValues = this.mappedValues === 'undefined';
 
     const svg = document.createElementNS(this.svgns, 'svg');
-      svg.setAttribute('id', this.data.group.name);
+      svg.setAttribute('id', this.data.name);
       svg.setAttribute('width', this.width);
-      svg.setAttribute('height', isNotMappedValues ? this.data.group.totalValue : this.mappedValues.get(this.data.group.totalValue));
+      svg.setAttribute('height', isNotMappedValues ? this.data.totalValue : this.mappedValues.get(this.data.totalValue));
       svg.style.margin = margin;
     
     let y = 0;
-    this.data.group.categories.forEach((d) => {
+    this.data.categories.forEach((d) => {
       let rect = document.createElementNS(this.svgns, 'rect');
         rect.setAttribute('id', d.name)
         rect.setAttribute('x', 0);
--- main.js
+++ main.js
@@ -1,87 +1,26 @@
-import Chart from "./components/Chart.js";
+import StackedBarChart from "./components/StackedBarChart.js";
 
-const group1 = {
-  "group" : {
-    "name": "Group_1",
-    "totalValue": 600,
-    "categories": [
-      {
-        "name": "Category_1",
-        "value": 100,
-        "color": "#769976"
-      },
-      {
-        "name": "Category_2",
-        "value": 200,
-        "color": "#A5E6A6"
-      },
-      {
-        "name": "Category_3",
-        "value": 300,
-        "color": "#E6E49A"
-      }
-    ]
+async function getGroup() {
+  const response = await fetch('./data/group.json');
+  const json = await response.json();
+  console.log(json);
+  const div = document.getElementById('stacked-bar-chart');
+  const {name, totalValue, categories} = json.group;
+  
+  const obj = {
+    name : name,
+    totalValue: totalValue,
+    categories: categories,
   }
-};
+  const bar = new StackedBarChart(obj);
+  bar.setWidth(250);
+  //creare una feature che in base al totale ti scala automaticamente il grafico per valori alti
+  bar.scaleValues(4, true);
+  bar.getStackedBarChart(10);
+  div.appendChild(bar.svg);
+}
 
-const group2 = {
-  "group" : {
-    "name": "Group_2",
-    "totalValue": 800,
-    "categories": [
-      {
-        "name": "Category_1",
-        "value": 200,
-        "color": "#A5E6A6"
-      },
-      {
-        "name": "Category_2",
-        "value": 300,
-        "color": "#E6E49A"
-      },
-      {
-        "name": "Category_3",
-        "value": 300,
-        "color": "#769976"
-      }
-    ]
-  }
-};
+getGroup();
 
-const group3 = {
-  "group" : {
-    "name": "Group_2",
-    "totalValue": 970,
-    "categories": [
-      {
-        "name": "Category_1",
-        "value": 200,
-        "color": "#A5E6A6"
-      },
-      {
-        "name": "Category_2",
-        "value": 350,
-        "color": "#E6E49A"
-      },
-      {
-        "name": "Category_3",
-        "value": 200,
-        "color": "#769976"
-      },
-      {
-        "name": "Category_4",
-        "value": 220,
-        "color": "#A69A73"
-      }
-    ]
-  }
-};
-const groups = [group1, group2, group3];
-const chart = new Chart(groups);
-let max = chart.getMax();
-chart.getGroups(50, 4, true, 10, max);
 
-const div = document.getElementById('stacked-bar-chart');
-chart.stackedBar.forEach(s => {
-  div.appendChild(s.svg);
-}); 
+
